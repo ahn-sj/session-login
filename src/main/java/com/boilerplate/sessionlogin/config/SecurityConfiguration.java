e96a7f6aed961a21d4dcdd211a685c850ec46a13
ahn-sj <jaesa5221@gmail.com>
2023-11-04 11:26:35 +0900

    test: github bot -> user.name

--- src/main/java/com/boilerplate/sessionlogin/config/SecurityConfiguration.java
+++ src/main/java/com/boilerplate/sessionlogin/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
